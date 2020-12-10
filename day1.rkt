01e35fdbc1d23d5e8872b3ac8da695f8acc84268
Paul Tan <pjxtan@gmail.com>
2020-12-10 17:51:21 -0500

    Use maybe

--- day1.rkt
+++ day1.rkt
@@ -1,34 +1,30 @@
 #lang racket
 
-(require "utils.rkt")
+(require data/maybe "utils.rkt")
 
 (provide part1 part2)
 
 (define input (load-ints "input/d1"))
 
-(define (part1) (match (find-pair input 2020)
-                       [(list x y) (* x y)]
-                       [_ 'no-solution]))
+(define (part1) (foldl * 1 (from-just! (find-pair input 2020))))
 
-(define (part2) (match (find-triplet input 2020)
-                       [(list x y z) (* x y z)]
-                       ['no-triplet 'no-solution]))
+(define (part2) (foldl * 1 (from-just! (find-triplet input 2020))))
 
 (define (find-pair entries target-sum)
   (match entries
-         ['() 'no-pair]
+         ['() nothing]
          [_ (let* ([x (first entries)] 
                    [xs (rest entries)] 
                    [y (- target-sum x)]) 
               (cond 
-                [(member y xs) (list x y)] 
+                [(member y xs) (just (list x y))] 
                 [else (find-pair xs target-sum)]))]))
 
 (define (find-triplet entries target-sum)
   (match entries
-         ['() 'no-triplet]
+         ['() nothing]
          [_ (let* ([x (first entries)]
                    [xs (rest entries)]) 
               (match (find-pair xs (- target-sum x)) 
-                     [(list y z) (list x y z)] 
-                     ['no-pair (find-triplet xs target-sum)]))]))
+                     [(just (list y z)) (just (list x y z))] 
+                     [nothing (find-triplet xs target-sum)]))]))
